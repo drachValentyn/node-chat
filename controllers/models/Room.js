4014b259f677376520ac7e84b9a47eb3da35dfae
Marina <marina.todorishina@gmail.com>
2020-04-11 02:36:11 +0300

    add new fields to models

--- controllers/models/Room.js
+++ controllers/models/Room.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const RoomSchema = new mongoose.Schema({
   room_name: String,
+  room_type: {type: String, default: 'none'},
   created_date: { type: Date, default: Date.now },
 });
 
